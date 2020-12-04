d346715c3752c4899f40e3342dbd8e243a20566f
Costel Corbuleac <costel.corbuleac@luminoslabs.com>
2020-12-04 22:02:05 +0200

    Module3-4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,14 +2,18 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <li v-for="book in books" :key="book.title">{{book.title}} : {{book.author}}</li>
+            <book-item v-for="book in books" :key="book.title" :book="book"></book-item>
         </ul>
     </div>
 </template>
 
 <script>
+
+import BookItem from "./BookItem"
+
 export default {
     name: "BookList",
+    components: { BookItem },
     data() {
         return {
             title: "All Books",
@@ -31,10 +35,5 @@ h1, h2 {
 ul {
     list-style-type: none;
     padding: 0;
-}
-
-li {
-    display: block;
-    margin: 0 10px;
 }
 </style>
